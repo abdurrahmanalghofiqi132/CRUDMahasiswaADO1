0465432e9c30ea8e36fbfe6614f1d0f1e3e5a432
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2026-05-08 00:22:31 +0700

    mengambil data yang sedang di klik alamat nama prodi dan berisi fungsi yang sudah bawaaan dri visual studio

--- CRUDMahasiswaADO1/Form1.cs
+++ CRUDMahasiswaADO1/Form1.cs
@@ -251,4 +251,26 @@ namespace CRUDMahasiswaADO1
                 comboBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["jenis_kelamin"].Value.ToString();
                 dateTimePicker1.Value = Convert.ToDateTime(
                     dataGridView1.Rows[e.RowIndex].Cells["tanggal_lahir"].Value);
-               
+                textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["alamat"].Value.ToString();
+                textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells["nama_prodi"].Value.ToString();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e) { }
+        private void label6_Click(object sender, EventArgs e) { }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+        private void textBox4_TextChanged(object sender, EventArgs e) { }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e) { }
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e) { }
+    }
+}
+
